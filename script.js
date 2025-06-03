40fa1eb308e20bf1426ccf2ceed33c51ac5f5d63
Notford <kwame.notford@gmail.com>
2025-06-03 01:15:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 50);
+  });
+});
